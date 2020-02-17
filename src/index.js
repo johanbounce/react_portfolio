3e0c680995b23a3f002763673c86e3212a7355af
bounce <johan@waytogo.se>
2020-02-17 15:00:13 +0100

    continuing setup following course documentation

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const HelloWorld = () => {
+  return <h2>Hello World</h2>;
+};
+
+ReactDOM.render(<HelloWorld />, document.getElementById("app"));
