36d4cb7224cf90ccd6c858bbe08808ecc2f569d8
bounce <johan@waytogo.se>
2020-02-17 15:32:10 +0100

    Fixing setup making hello world page work

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Hello from "./Hello";
 
-const HelloWorld = () => {
-  return <h2>Hello World</h2>;
+const App = () => {
+  return <Hello />;
 };
 
-ReactDOM.render(<HelloWorld />, document.getElementById("app"));
+
+ReactDOM.render(<App />, document.getElementById("app"));
