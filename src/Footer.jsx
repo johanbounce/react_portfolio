2d8cf9b02cba4677580de04a911a9e963f1217fc
bounce <johan@waytogo.se>
2020-02-18 10:36:11 +0100

    Creating test in cypress for header, footer and main container. Creating Header, Footer and Hello components

---
+++ src/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="ui container">
+        <p id="footer">Made with React {React.version}</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <nav className="ui fixed inverted menu">
+      <div className="ui container">
+        <h3 id="header" className="header item">My Portfolio</h3>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
