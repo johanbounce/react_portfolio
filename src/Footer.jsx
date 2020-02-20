599409ffe15ef0f639135396df05ad3fae204665
bounce <johan@waytogo.se>
2020-02-20 16:41:02 +0100

    footer with social links

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { Icon, Popup } from 'semantic-ui-react';
 
 const Footer = () => {
   return (
     <footer>
       <div style={{borderTopStyle: "solid", borderColor: "white", borderWidth: 3}}  className="ui basic center aligned segment">
-        <p id="footer">Made with React {React.version}</p>
+      <p>
+        <a href="https://instagram.com/johanbounce/" target="_blank"><i style={{color: "white"}} class="large instagram link icon"/></a>
+        <a href="https://se.linkedin.com/in/johan-bons-10b420153" target="_blank"><i style={{color: "white"}} class="large linkedin link icon"/></a>
+        <a href="mailto:johan@waytogo.se" target="_blank"><i style={{color: "white"}} class="large mail link icon"/></a>
+        <a href="https://waytogo.se/" target="_blank"><i style={{color: "white"}} class="large green heart link icon"/></a>
+      </p>
       </div>
     </footer>
   );
