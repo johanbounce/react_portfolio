ad73d9f89c5bd8bed52e345fd5f10446934133d9
bounce <johan@waytogo.se>
2020-02-21 08:47:33 +0100

    done for challenge week 3

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -14,7 +14,6 @@ const Footer = () => {
         content="Send me an email!"
         size="small"
         position='bottom left'
-        open
         inverted
         ></Popup>
         <a href="https://waytogo.se/" target="_blank"><i style={{color: "white"}} class="large green heart link icon"/></a>
